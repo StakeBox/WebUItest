357d14ed99b7948de3fb01735ee5214bc8816854
StakeBox <support@stakebox.com>
2015-09-10 18:23:30 -0400

    .

--- WebUI/last20transactions.php
+++ WebUI/last20transactions.php
@@ -49,6 +49,16 @@ $x = array_reverse($trans);
 		<td>{$x['comment']}</td>
                 </tr>";
 	  }
+	  else {
+            echo "<td>{$x['address']}</td>
+                <td><div style='width:60px;overflow:hidden'>{$x['account']}</div></td>
+                <td><div style='width:70px;overflow:hidden'><font color='{$coloramount}'>{$x['amount']}</font></div></td>
+		<td><div style='width:110px;overflow:hidden'><font color='{$colorconfirms}'>{$x['confirmations']}</font></div></td>
+                <td>{$date}</td>
+                <td><div style='width:120px;overflow:hidden'>{$x['txid']}</div></td>
+		<td></td>
+                </tr>";
+	  }
         }
         echo "</table>";
         ?>
