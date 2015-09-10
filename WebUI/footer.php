b63f25b2b91d7bb6c71db0b0d598c2a9d48a1d38
StakeBox <support@stakebox.com>
2015-09-10 09:38:32 -0400

    .

--- WebUI/footer.php
+++ WebUI/footer.php
@@ -4,19 +4,9 @@
 		</div><!--.container-->
 </pre></body></html>
 <script>
-var myVar=setInterval(function(){price()},1000);
-
-function price() {
-    
-	<?php // fetch price in BTC price of current coin
-	$curl = curl_init();
-	curl_setopt($curl, CURLOPT_URL, "https://www.cryptonator.com/api/full/".$pair);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	$rawDatab = curl_exec($curl);
-	curl_close($curl);
-	$datab = json_decode($rawDatab);
-	$priceb = $datab->ticker->markets[0]->price;
-	?>
-	document.title = <?php print($priceb); ?>;
-}
+     setInterval(function () {
+        $.get("price.php", function() {
+                document.title($priceb);
+        });
+    }, 3000);
 </script>
---
+++ WebUI/price.php
@@ -0,0 +1,10 @@
+<?php 
+	// fetch price in BTC price of current coin
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, "https://www.cryptonator.com/api/full/".$pair);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	$rawDatab = curl_exec($curl);
+	curl_close($curl);
+	$datab = json_decode($rawDatab);
+	$priceb = $datab->ticker->markets[0]->price;
+?>
