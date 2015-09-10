daab2fc8bcaedc8a36b73cc1c8a0b263e9a4c54c
StakeBox <support@stakebox.com>
2015-09-10 15:32:35 -0400

    .

--- WebUI/footer.php
+++ WebUI/footer.php
@@ -4,7 +4,7 @@
 		</div><!--.container-->
 </pre></body></html>
 <script>
-	var pair = <?php echo json_encode($pair); ?>;
+
 
 	var data_from_ajax = "HEY";
 
@@ -13,7 +13,7 @@
 		document.getElementById("output").innerHTML = data_from_ajax;
 
 
-	document.title = data_from_ajax:
+
 </script>
 <script>
 	var name = "kieran";
