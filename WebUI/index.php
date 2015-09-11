49d59bea9584e0e16cd2f139f5b16463c83a4a77
StakeBox <support@stakebox.com>
2015-09-11 14:10:40 -0400

    .

--- WebUI/index.php
+++ WebUI/index.php
@@ -96,7 +96,7 @@ if ($currentWallet == BottleCaps || $currentWallet == HoboNickels || $currentWal
    else{
 	echo "<p>nope</p>";
    }
-   print_r($rawData2); 
+   print_r($latest); 
    ?>
 	<div class="col-lg-7">
 	<p> Your main wallet address is <?php print_r($address); ?>.    
