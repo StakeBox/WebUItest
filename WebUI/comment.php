8dda6cfe767f581906cb4d92c72aa6dc4120e2b7
StakeBox <support@stakebox.com>
2015-09-10 20:50:41 -0400

    .

--- WebUI/comment.php
+++ WebUI/comment.php
@@ -42,12 +42,13 @@ if($_POST['comment'] != ""){
         <form action="comment" method="POST"><input type="hidden">
             <input type="hidden" name="a">
                 <tr>
-                    <td><b>To Address</b></td>
+                    <td><b>Comment:</b></td>
                     <td><input class="form-control" type="text" name="address" value="<?php print($address); ?>"></td>
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
                     <td><button class='btn btn-default' type="submit" value="Preview"><b>Preview</b></button></td>
+		</tr>
         </form>
     </table>
 </div>
