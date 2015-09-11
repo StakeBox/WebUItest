d4d744b1abf9c12a9f196b4c4a10245462178168
StakeBox <support@stakebox.com>
2015-09-10 23:18:49 -0400

    .

--- WebUI/index.php
+++ WebUI/index.php
@@ -120,9 +120,9 @@ if ($currentWallet == BottleCaps || $currentWallet == HoboNickels || $currentWal
 	    <label for="notes">Notes:</label>
 	      <textarea class="form-control" name="notes" id="notes" cols="60" rows="10"></textarea>
 		<button class='btn btn-default' type="submit" value="setprimary">Save Notes</button>
-	  </div>
 	  </form>
-	 </div>
+	  </div>
+	</div>
 </div>
 </div>
 <?php include ("footer.php"); ?>
