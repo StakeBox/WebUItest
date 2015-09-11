84ba5128af26c6153c37e1a80b5aa30e4d91d493
StakeBox <support@stakebox.com>
2015-09-10 20:48:50 -0400

    .

--- WebUI/comment.php
+++ WebUI/comment.php
@@ -36,5 +36,20 @@ if($_POST['comment'] != ""){
 }
 	
 ?>
-
+<div class="panel panel-default">
+  <center><p>Add comment to transaction <?php echo $_POST['txid']; ?>
+    <table class="table-hover table-condensed table-bordered table">
+        <form action="comment" method="POST"><input type="hidden">
+            <input type="hidden" name="a">
+                <tr>
+                    <td><b>To Address</b></td>
+                    <td><input class="form-control" type="text" name="address" value="<?php print($address); ?>"></td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><button class='btn btn-default' type="submit" value="Preview"><b>Preview</b></button></td>
+        </form>
+    </table>
+</div>
+</div>
 <?php include ("footer.php"); ?>
