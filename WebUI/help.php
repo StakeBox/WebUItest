c425b6f3ad2083d4ccf255757b027bf1e83223bb
StakeBox <support@stakebox.com>
2015-11-05 13:17:19 -0500

    v1.2.2

--- WebUI/help.php
+++ WebUI/help.php
@@ -1,11 +1,12 @@
 <?php
 include ("header.php");
 include ("pass.php");
-
+?>
 <p>
    This page is here to describe all of the functions of the WebUI, and give 
    some pointers on best practices.
 </p>
 </div>
+<?php
 include ("footer.php");
 ?>
