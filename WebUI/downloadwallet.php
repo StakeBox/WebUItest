042e7472a5f13f12c07cc8a7f433cc180ed2d46a
StakeBox <support@stakebox.com>
2015-11-05 09:58:38 -0500

    v1.2.2

--- WebUI/downloadwallet.php
+++ WebUI/downloadwallet.php
@@ -2,6 +2,8 @@
 include ("header.php");
 include ("pass.php");
 $destination = "/var/www/html/webui/WebUI/".$currentWallet."wallet.dat";
+// create a new cURL resource
+$ch = curl_init();
 ?>
 
 <?php 
@@ -12,6 +14,10 @@ try {
 	echo "<p class='bg-danger'><b>Error: Something went wrong...  Did you enter a location that does not exist, or is unreachable?</b></p>";
 }
 exec("sudo -u stakebox chmod g+r /var/www/html/webui/WebUI/".$currentWallet."wallet.dat");
+curl_setopt($ch, CURLOPT_URL, "http://stakebox.local/HYPERwallet.dat");
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_exec($ch);
+curl_close($ch);
 ?>
 </div>
 <?php include ("footer.php");?>
