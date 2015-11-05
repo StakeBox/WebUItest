a3ee6471757aa4ef6edd3641e0b6dc3b197362a8
StakeBox <support@stakebox.com>
2015-11-05 10:01:20 -0500

    v1.2.2

--- WebUI/downloadwallet.php
+++ WebUI/downloadwallet.php
@@ -13,7 +13,7 @@ try {
 } catch(Exception $e) {
 	echo "<p class='bg-danger'><b>Error: Something went wrong...  Did you enter a location that does not exist, or is unreachable?</b></p>";
 }
-exec("sudo -u stakebox chmod g+r /var/www/html/webui/WebUI/".$currentWallet."wallet.dat");
+
 curl_setopt($ch, CURLOPT_URL, "http://stakebox.local/HYPERwallet.dat");
 curl_setopt($ch, CURLOPT_HEADER, false);
 curl_exec($ch);
