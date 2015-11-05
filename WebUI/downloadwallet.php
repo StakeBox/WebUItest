68d4547544d67fae9e1b5d62be8ea153fb971ed8
StakeBox <support@stakebox.com>
2015-11-05 10:42:08 -0500

    v1.2.2

--- WebUI/downloadwallet.php
+++ WebUI/downloadwallet.php
@@ -5,12 +5,7 @@ $destination = "/var/www/html/webui/WebUI/".$currentWallet."wallet.dat";
 ?>
 
 <?php 
-try {
-	$coin->backupwallet($destination);
-	echo "<p class='bg-success'><b>You have successfully backed your wallet up to $destination.</b></p> ";
-} catch(Exception $e) {
-	echo "<p class='bg-danger'><b>Error: Something went wrong...  Did you enter a location that does not exist, or is unreachable?</b></p>";
-}
+$coin->backupwallet($destination);
 exec("sudo chmod g+r /var/www/html/webui/WebUI/".$currentWallet."wallet.dat");
 header("Location: http://stakebox.local/".$currentWallet."wallet.dat");
 exit;
