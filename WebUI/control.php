a0cfe3653052a6b549a6a359aaacb29e33021418
StakeBox <support@stakebox.com>
2015-11-05 11:31:01 -0500

    v1.2.2

--- WebUI/control.php
+++ WebUI/control.php
@@ -315,7 +315,7 @@ include ("pass.php");
 <!-- The data encoding type, enctype, MUST be specified as below -->
 <form enctype="multipart/form-data" action="upload" method="POST">
     <!-- MAX_FILE_SIZE must precede the file input field -->
-    <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+    <input type="hidden" name="MAX_FILE_SIZE" value="300000" />
     <!-- Name of input element determines name in $_FILES array -->
     Send this file: <input name="userfile" type="file" />
     <input type="submit" value="Send File" />
