317df8f005e782d91de5ad32048dfae459db0225
StakeBox <support@stakebox.com>
2015-11-06 09:15:06 -0500

    v1.2.2

--- WebUI/header.php
+++ WebUI/header.php
@@ -84,10 +84,40 @@
 	    }
 	}
 
+$lockStateLocation = "/home/stakebox/UI/".$currentWallet."lockstate.php";
+
+function changeLockState(){
+
+	global $lockStateLocation;
+	global $newLockState;
+	if(!file_exists("$lockStateLocation")){
+		$file = fopen("$lockStateLocation","w");
+		fwrite($file,"");
+		fclose($file);
+	}   
+	if (is_readable($lockStateLocation) == FALSE) 
+		die ("The lock state file must be writable.") ; 
+
+	// Open the file and erase the contents if any
+	$fp = fopen($lockStateLocation, "w");
+
+	// Write the data to the file
+	// CODE INJECTION WARNING!
+  	fwrite($fp, "<?php\n\$lockState='$newLockState';\n?>");	  	
+  	// Close the file
+  	fclose($fp);
+}
+
 	include("/home/stakebox/UI/version.php");
 	include("/home/stakebox/UI/primary".$currentWallet."address.php");
 	if (!file_exists("/home/stakebox/UI/".$currentWallet."lockstate.php")) {
-		$lockState = "Not Encrypted";
+		try {
+			$coin->walletlock();
+			$newLockState = "Locked";
+			changeLockState();
+		} catch(Exception $e) {	
+			$lockState = "Not Encrypted";
+		}
 	}
 	else {
 		include("/home/stakebox/UI/".$currentWallet."lockstate.php");
