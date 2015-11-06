3eb05ed771d1136681bc00ce5569bade6a6a2432
StakeBox <support@stakebox.com>
2015-11-06 18:44:24 -0500

    v1.2.2

--- WebUI/gettransaction.php
+++ WebUI/gettransaction.php
@@ -8,6 +8,6 @@ include ("pass.php");
 </div>
 <div class="well"> 
 <p><b>This is the complete information for the transaction with txid <?php print_r($_POST['txid']);?>.</b></p>
-<?php printarray($coin->gettransaction($_POST['txid'])); ?>
+
 </div>
 <?php include ("footer.php");?>
